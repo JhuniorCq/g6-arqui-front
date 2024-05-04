5a732bd6debcd6260c399fd3f426f198083fe23f
Almabade1 <balcedamariana@gmail.com>
2024-05-03 22:50:53 -0500

    Modificación busqueda navbar

--- front-end/src/components/Navbar/NavbarPostulant.jsx
+++ front-end/src/components/Navbar/NavbarPostulant.jsx
@@ -10,7 +10,7 @@ export const NavbarPostulant = () => {
   const authContext = useContext(AuthContext);
   const { user } = authContext;
   const id = useId();
-  const navigate= useNavigate();
+  const navigate = useNavigate();
   //Funcion para validar que el input de busqueda tenga un texto (probando la funcion)
   const { manejarCambiosForm, state } = useForm({
     [`${id}-busqueda`]: "hh",
@@ -18,23 +18,28 @@ export const NavbarPostulant = () => {
   const mifuncion = (event) => {
     event.preventDefault();
     console.log(state);
-    const valorBusqueda= state[`${id}-busqueda`]
-    valorBusqueda? navigate("/ofertas-empleo"):alert("Ingrese una busqueda ps sonso")
+    const valorBusqueda = state[`${id}-busqueda`];
+    valorBusqueda
+      ? navigate("/ofertas-empleo")
+      : alert("Ingrese una busqueda ps sonso");
   };
   return (
     <header className={styles.header}>
-      <nav>
+      <nav className={styles.nav}>
         <ul className={styles.lista}>
-          <li>
-            <Link to="/" className={styles.enlace}>
-              Logo
-            </Link>
-          </li>
-          <li>
-            <div className="busque">
-              <form onSubmit={mifuncion} className="contenedor-input1">
-                //Es el className para el input className=
-                {`${styles.inputCarrera1} ${styles.anchoTotal1}`}
+          <div className={styles.leftItems}>
+            {/* De ahí cambio este ESTILO */}
+            <li>
+              <Link to="/publicar-ofertas" className={styles.enlace}>
+                Logo
+              </Link>
+            </li>
+          </div>
+          <div className={styles.centerItems}>
+            <li className={styles.busque}>
+              <form onSubmit={mifuncion} className={styles.form}>
+                {/* Es el className para el input className=
+                {`${styles.inputCarrera1} ${styles.anchoTotal1}`} */}
                 //No funciona
                 {/* <Input 
                   type="text" 
@@ -54,47 +59,43 @@ export const NavbarPostulant = () => {
                   className={styles.inputCarrera1}
                   onChange={manejarCambiosForm}
                 />
-                <button>
+                <button type="submit" className={styles.searchButton}>
                   <FaSearch />
                 </button>
                 {/* <Link onClick={mifuncion} to="/" className={styles.iconoBoton}></Link> */}
-              </form>
-              <div className="contenedor-select1">
                 <select
-                  name=""
-                  id="input-lugar1"
-                  className="input-lugar1 ancho-total2"
+                  name="{`${id}-select`}"
+                  id={`${id}-select`}
+                  className={styles.inputLugar1}
                 >
                   <option value="">Lugar</option>
                   <option value="">Lima</option>
                   <option value="">Ica</option>
                   <option value="">Huancayo</option>
-
                   <option value="">Puno</option>
                   <option value="">Cusco</option>
                 </select>
-              </div>
-            </div>
-          </li>
-          <li className="derecha">
-            <NavLink
-              to="/mis-postulaciones"
-              className={({ isActive }) =>
-                isActive
-                  ? `${styles.activo} ${styles.enlace}`
-                  : `${styles.enlace}`
-              }
-            >
-              Mis Postulaciones
-            </NavLink>
-          </li>
-          <li>
-            <Dropdown
-              className="derecha"
-              userOptions={true}
-              nombre={user.name}
-            />
-          </li>
+              </form>
+            </li>
+          </div>
+
+          <div className={styles.rightItems}>
+            <li>
+              <NavLink
+                to="/mis-postulaciones"
+                className={({ isActive }) =>
+                  isActive
+                    ? `${styles.activo} ${styles.enlace}`
+                    : `${styles.enlace}`
+                }
+              >
+                Mis Postulaciones
+              </NavLink>
+            </li>
+            <li>
+              <Dropdown userOptions={true} nombre={user.name} />
+            </li>
+          </div>
         </ul>
       </nav>
     </header>
